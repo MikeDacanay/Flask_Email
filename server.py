736163e6c90c9f6b600a8c2eac738b13ee2a9971
Mike Dacanay <migd64@gmail.com>
2017-08-10 00:53:54 -0700

    input text email properly behaving to include in the database if it is allowable

--- server.py
+++ server.py
@@ -15,27 +15,29 @@ def index():
 def check():
 	name = request.form['email']
 	data_emails = mysql.query_db("SELECT * FROM emails")
+	count=0
 
 	if len(name) < 1:
 		flash("Blank Email Address!")
+		count=1
 	elif not EMAIL_REGEX.match(name):
 		flash("Invalid Email Address!")	
+		count=1
 	else:
 		for x in data_emails:
 			if x['email']==name:
 				flash("Email is already in use!")
+				count=1
 				break
 
-	if flash!= None:
-		print "it worked"
-	# if counter=0:
-	# 	query="INSERT INTO emails(email,c_date) VALUES(:name2,NOW())"
+	if count==0:
+		query="INSERT INTO emails(email,c_date) VALUES(:name2,NOW())"
 
-	# 	data= {
-	# 		'name2':name
-	# 	}
+		data= {
+			'name2':name
+		}
 
-	# 	mysql.query_db(query, data)
+		mysql.query_db(query, data)
 			
 	return redirect('/')
 
