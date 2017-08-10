c939c2e916cb1ac82bb61b365734f9aa5a712ae1
Mike Dacanay <migd64@gmail.com>
2017-08-10 00:48:10 -0700

    testing if we can test value of flash when flash() is called

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, redirect, flash
 from mysqlconnection import MySQLConnector
+import re
 app = Flask(__name__)
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 app.secret_key= "Jade"
 
 mysql = MySQLConnector(app, 'email_db')
@@ -12,7 +14,29 @@ def index():
 @app.route('/validator', methods=['POST'])
 def check():
 	name = request.form['email']
-	print name
+	data_emails = mysql.query_db("SELECT * FROM emails")
+
+	if len(name) < 1:
+		flash("Blank Email Address!")
+	elif not EMAIL_REGEX.match(name):
+		flash("Invalid Email Address!")	
+	else:
+		for x in data_emails:
+			if x['email']==name:
+				flash("Email is already in use!")
+				break
+
+	if flash!= None:
+		print "it worked"
+	# if counter=0:
+	# 	query="INSERT INTO emails(email,c_date) VALUES(:name2,NOW())"
+
+	# 	data= {
+	# 		'name2':name
+	# 	}
+
+	# 	mysql.query_db(query, data)
+			
 	return redirect('/')
 
 
